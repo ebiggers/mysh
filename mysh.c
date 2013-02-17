1b06ba1f5ae7397148a49aa753e4541cee61d9c2
Eric Biggers <ebiggers3@gmail.com>
2013-02-17 17:01:24 -0600

    builtin_cd()

--- mysh.c
+++ mysh.c
@@ -351,7 +351,7 @@ struct orig_fds {
 
 static int undo_redirections(const struct orig_fds *orig)
 {
-	int ret = true;
+	int ret = 0;
 	if (orig->orig_stdin >= 0)
 		if (dup2(orig->orig_stdin, STDIN_FILENO) < 0)
 			ret = -1;
@@ -521,43 +521,120 @@ fail:
 	exit(-1);
 }
 
+static int builtin_pwd(int argc, const char **argv)
+{
+	char *buf;
+	int ret = -1;
+
+	buf = getcwd(NULL, 0);
+	if (!buf) {
+		mysh_error_with_errno("pwd: can't get current working directory");
+		goto out;
+	}
+	if (puts(buf) == EOF)
+		mysh_error_with_errno("pwd: write error");
+	else
+		ret = 0;
+	free(buf);
+out:
+	return ret;
+}
+
+static int builtin_cd(int argc, const char **argv)
+{
+	int ret;
+	const char *dest_dir;
+
+	if (argc < 2) {
+		dest_dir = getenv("HOME");
+		if (!dest_dir) {
+			mysh_error("cd: HOME not set");
+			ret = 1;
+			goto out;
+		}
+	} else {
+		dest_dir = argv[1];
+	}
+	if (chdir(dest_dir) != 0) {
+		mysh_error_with_errno("cd: %s", dest_dir);
+		ret = 1;
+	} else {
+		ret = 0;
+	}
+out:
+	return ret;
+}
+
 struct builtin {
 	const char *name;
+	int (*func)(int argc, const char **argv);
 };
 
 static const struct builtin builtins[] = {
-	{"pwd"},
-	{"cd"},
-	{"setenv"},
-	{"getenv"},
-	{"exit"},
+	{"pwd", builtin_pwd},
+	{"cd", builtin_cd},
+	{"setenv", NULL},
+	{"getenv", NULL},
+	{"exit", NULL},
 };
 
 #define NUM_BUILTINS ARRAY_SIZE(builtins)
 
-#define for_builtin(b) \
-		for (b = builtins; b != builtins + NUM_BUILTINS; b++)
-
-#if 0
-static bool execute_builtin(const struct token *command_toks,
-			    const struct redirections *redirs,
-			    const unsigned command_nargs,
-			    int *status_ret)
+static int execute_builtin(const struct builtin *builtin,
+			   const struct token *command_toks,
+			   const struct token *redirs,
+			   unsigned cmd_nargs)
 {
-	const struct builtin *b;
-	const char *name = command_toks->tok_data;
-	int status = -1;
+	struct orig_fds orig = {-1, -1};
+	const char *argv[cmd_nargs + 1];
+	const struct token *tok;
+	unsigned i;
+	int status;
+	int ret;
 
-	for_builtin(b)
-		if (strcmp(b->name, name) == 0)
-			goto found_builtin;
-	/* not a builtin command */
-	return false;
-found_builtin:
-	*status_ret = status;
-	return true;
+	/* Do redirections for the builtin */
+	status = do_redirections(redirs, &orig);
+	if (status)
+		return status;
+
+	/* Prepare argv for the builtin */
+	tok = command_toks;
+	for (i = 0; i < cmd_nargs; i++, tok = tok->next)
+		argv[i] = tok->tok_data;
+	argv[i] = NULL;
+	/* Call the builtin function */
+	builtin->func(cmd_nargs, argv);
+
+	/* Undo redirections for the builtin */
+	ret = undo_redirections(&orig);
+	if (ret) {
+		if (status == 0)
+			status = ret;
+		mysh_error_with_errno("Failed to restore redirections");
+	}
+	return status;
 }
-#endif
+
+static bool maybe_execute_builtin(const struct token *command_toks,
+				  const struct token *redirs,
+				  unsigned cmd_nargs,
+				  int *status_ret)
+{
+	const char *name = command_toks->tok_data;
+	size_t i;
+
+	for (i = 0; i < NUM_BUILTINS; i++) {
+		if (strcmp(builtins[i].name, name) == 0 && builtins[i].func) {
+			/* The command matched a builtin.  Execute it. */
+			*status_ret = execute_builtin(&builtins[i],
+						      command_toks,
+						      redirs, cmd_nargs);
+			return true;
+		}
+	}
+	/* Not a builtin command */
+	return false;
+}
 
 static int execute_pipeline(const struct token * const *pipe_commands,
 			    unsigned ncommands)
@@ -624,14 +701,12 @@ static int execute_pipeline(const struct token * const *pipe_commands,
 
 	/* If the pipeline only has one command and is not being executed
 	 * asynchronously, try interpreting the command as a builtin. */
-#if 0
 	if (ncommands == 1 || !async) {
-		if (execute_builtin(pipe_commands[0], &redirs[0],
-				    command_nargs[0], &ret))
+		if (maybe_execute_builtin(pipe_commands[0], redirs[0],
+					  command_nargs[0], &ret))
 		    return ret;
 		/* not a builtin */
 	}
-#endif
 
 	/* Execute the commands */
 	prev_read_end = pipe_fds[0] = pipe_fds[1] = -1;
